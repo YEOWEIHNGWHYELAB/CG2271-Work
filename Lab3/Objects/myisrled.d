f82b65aab984cedfcbb9b9d07a6b1c4af3163c03
YEOWEIHNGWHYELAB <yeoweihngywh@gmail.com>
2022-02-07 01:42:55 +0800

    Toggle Blinky or Off

---
+++ Lab3/Objects/myisrled.d
@@ -0,0 +1,8 @@
+.\objects\myisrled.o: myISRLED.c
+.\objects\myisrled.o: C:\Users\WHYELAB\AppData\Local\Arm\Packs\Keil\Kinetis_KLxx_DFP\1.15.0\Device\Include\MKL25Z4.h
+.\objects\myisrled.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\myisrled.o: C:\Users\WHYELAB\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\core_cm0plus.h
+.\objects\myisrled.o: C:\Users\WHYELAB\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_version.h
+.\objects\myisrled.o: C:\Users\WHYELAB\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_compiler.h
+.\objects\myisrled.o: C:\Users\WHYELAB\AppData\Local\Arm\Packs\ARM\CMSIS\5.6.0\CMSIS\Core\Include\cmsis_armcc.h
+.\objects\myisrled.o: .\RTE\Device\MKL25Z128xxx4\system_MKL25Z4.h
--- Lab3/myISRLED.c
+++ Lab3/myISRLED.c
@@ -15,7 +15,7 @@ typedef enum led_colors {
 unsigned int counter = 0;
 char led_mapping[3][2] = {{0, red_led}, {1, green_led}, {2, blue_led}};
 char led_control = 1;
-char int_count = 0;
+int int_count = 0;
 
 /* GPIO Initialization Function */
 void initGPIO(void) {
@@ -23,6 +23,7 @@ void initGPIO(void) {
 	SIM->SCGC5 |= ((SIM_SCGC5_PORTB_MASK) | (SIM_SCGC5_PORTD_MASK));
 	
 	// Configure MUX settings to make all 3 pins GPIO
+	// MUX(1) is always GPIO by default
 	PORTB->PCR[RED_LED] &= ~PORT_PCR_MUX_MASK;
 	PORTB->PCR[RED_LED] |= PORT_PCR_MUX(1);
 	PORTB->PCR[GREEN_LED] &= ~PORT_PCR_MUX_MASK;
@@ -30,7 +31,7 @@ void initGPIO(void) {
 	PORTD->PCR[BLUE_LED] &= ~PORT_PCR_MUX_MASK;
 	PORTD->PCR[BLUE_LED] |= PORT_PCR_MUX(1);
 	
-	// Set Data Direction Registers for PortB and PortD
+	// Set Data Direction Registers for PortB and PortD LEDs
 	PTB->PDDR |= (MASK(RED_LED) | MASK(GREEN_LED));
 	PTD->PDDR |= MASK(BLUE_LED);
 }
@@ -42,8 +43,7 @@ void offRGB(void) {
 	PTD->PSOR = MASK(BLUE_LED);
 }
 
-void ledToggle(char colour)
-{
+void ledToggle(char colour) {
 	switch(led_mapping[colour][1])
 	{
 		case RED_LED:
@@ -68,58 +68,61 @@ static void delay(volatile uint32_t nof) {
 	}
 }
 
-void initSwitch(void)
-{
-	// enable clock for PortD
+void initSwitch(void) {
+	// Enable clock for PortD
+	// Why do we enable it again? Its ok to do it 2 times due to 
+	// the |= bit masking technique, ensure you do not do a direct overwrite
+	// but the main purpose is to be able to reuse this function
+	// without additional code... 
 	SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
 	
-	/* Select GPIO and enable pull-up resistors and interrupts on
-	falling edges of pin connected to switch*/
+	// Select GPIO and enable pull-up resistors and interrupts on
+	// falling edges of pin connected to switch
+	// MUX(1) is for configuring GPIO
+	// IRQC determines how the interrupt is being triggered
+	// We want to interrupt on falling edge here and is 1010...
 	PORTD->PCR[SW_POS] |= (PORT_PCR_MUX(1) |
-												PORT_PCR_PS_MASK |
-												PORT_PCR_PE_MASK |
-												PORT_PCR_IRQC(0x0a));
+												PORT_PCR_PS_MASK | // Pull select the pullup instead of pulldown
+												PORT_PCR_PE_MASK | // Internal pullup register enabled
+												PORT_PCR_IRQC(0x0a)); // It will shift it by 16 and do a masking... Why shift by 16 is because IRQC is at 16, 17, 18, 19...  
 	
 	// Set PORT D Switch bit to input
-	PTD->PDDR &= ~MASK(SW_POS);
+	// Need to clear the bits first by &- and the ~
+	PTD->PDDR &= ~MASK(SW_POS); 
 	
-	//Enable Interrupts
-	NVIC_SetPriority(PORTD_IRQn, 2);
-	NVIC_ClearPendingIRQ(PORTD_IRQn);
-	NVIC_EnableIRQ(PORTD_IRQn);
+	// Enable Interrupts and config
+	// SW is connected to PORTD, you cannot do individual pin for a PORT, can only do for all!
+	NVIC_SetPriority(PORTD_IRQn, 2); // Can set to 0, 1, 2 as we don't have any other interrupts
+	NVIC_ClearPendingIRQ(PORTD_IRQn); // Legacy code, just in case if a bit already set for pending
+	NVIC_EnableIRQ(PORTD_IRQn); // PORTD_IRQn is 31 macro... Reduce magic number
 }
 
-void PORTD_IRQHandler()
-{
+void PORTD_IRQHandler() {
 	// Clear Pending IRQ
-	NVIC_ClearPendingIRQ(PORTD_IRQn);
+	NVIC_ClearPendingIRQ(PORTD_IRQn); // Not actually necessary as it will run clear automatically, for legacy...
 	
 	// Updating some variable / flag
 	int_count++;
-	if (int_count > 3) 
-	{
-		int_count = 0;
-	}
-	led_control ^= 1;
+	// if (int_count > 3) 
+	//	int_count = 0;
+	led_control ^= 1; // Toggle
 	
-	//Clear INT Flag
-	PORTD->ISFR |= MASK(SW_POS);
+	// Clear Interrupt Flag
+	PORTD->ISFR |= MASK(SW_POS); // Write a 1 to ISFR reg for that particular bit position. 
 }
 
 
-int main(void)
-{
-	SystemCoreClockUpdate();
+int main(void) {
+	// SystemCoreClockUpdate();
 	initSwitch();
 	initGPIO();
 	offRGB();
 	
 	while(1)
 	{
-		offRGB();
 		if(led_control)
 		{
-			ledToggle(int_count);
+			ledToggle(0); // Let it blink red or switch it off
 			delay(0x80000);
 		}
 		else 
